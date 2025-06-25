a57233538900050ca3267569fca94edcf39cb84d
kashyap kanumuri <kashyap.kanumuri@gmail.com>
2025-06-24 20:14:51 -0400

    two way communication established between Duet board and arduino. Need to properly reset pins after print resumes. Need to include clear indicators for when logging should resume and pause. Need to print in format that will be easy to convert to plots.

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,67 @@
 #include <Arduino.h>
 #include <CircularBuffer.h>
 
-CircularBuffer<int, 30> tempWindow;
+CircularBuffer<float, 20> tempWindow;
+
 const int tempPin = A0;
-float voltage = 0;
-float tempeInC = 0;
 const float resistor = 263.355;
 float a;
 float b;
+
 const int temp_1 = 0;
 const int temp_2 = 1300;
-int i = 0;
-const int inputPin = 0;
-const int outputPin = 0;
+
+const int outputPin = 3;
+const int inputPin = 4;
+
+bool stop_tracking = false;
+const float target_temp = 29;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(inputPin, INPUT);
   pinMode(outputPin, OUTPUT);
+  digitalWrite(outputPin, LOW);
+  pinMode(inputPin, INPUT);
   analogReadResolution(14);
+
   float raw1 = 16383 * (0.004 * resistor) / 5;
   float raw2 = 16383 * (0.02 * resistor) / 5;
-  a = (temp_2 - temp_1) / (raw2- raw1);
+  a = (temp_2 - temp_1) / (raw2 - raw1);
   b = temp_2 - a * raw2;
 }
 
 void loop() {
-  int rawValue = analogRead(tempPin);
-  float temperature = rawValue * a + b;
-  tempWindow.push(temperature);
-  if (!tempWindow.isFull()) {
-    Serial.println("Window not full.");
-  } else {
-    float sum = 0;
-    for (int i = 0; i < tempWindow.size(); i++) {
-      sum += tempWindow[i];
-    }
-    float average = sum / tempWindow.size();
-    Serial.println(average);
-    tempWindow.shift();
+  Serial.print("Pin 3: ");
+  Serial.print(digitalRead(outputPin));
+  Serial.print(" | Pin 4: ");
+  Serial.println(digitalRead(inputPin));
+
+  if (digitalRead(inputPin) == HIGH) {
+      int rawValue = analogRead(tempPin);
+      float temperature = rawValue * a + b;
+
+      tempWindow.push(temperature);
+
+      if (!tempWindow.isFull()) {
+        Serial.println("Window not full.");
+      } else {
+        float sum = 0;
+        for (int i = 0; i < tempWindow.size(); i++) {
+          sum += tempWindow[i];
+        }
+        float average = sum / tempWindow.size();
+        Serial.print("Average: ");
+        Serial.println(average);
+
+        if (average > target_temp) {
+          Serial.println("Target temperature hit, resuming print");
+          stop_tracking = true;
+          digitalWrite(outputPin, HIGH);
+        }
+
+        tempWindow.shift();
+      }
+
+      delay(1000);
   }
-  delay(400);
 }
