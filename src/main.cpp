a5a53aa577d9bdb326aa0bf31ac3c62123fe5299
King-Kash <kashyap.kanumuri@gmail.com>
2025-07-01 22:35:51 -0400

    minor updates to read temperature inputs from optrix thermal camera.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,87 @@
+// #include <Arduino.h>
+// #include <CircularBuffer.h>
+
+// CircularBuffer<float, 20> tempWindow;
+
+// const int tempPin = A0;
+// const float resistor = 263.6;
+// float a;
+// float b;
+
+// const int temp_1 = 0;
+// const int temp_2 = 1300;
+
+// const int outputPin = 3;
+// const int inputPin = 4;
+
+// // bool stop_tracking = false;
+// const float min_temp = 26;
+// const float max_temp = 30;
+
+// int currentLayer = 0;
+
+// void setup() {
+//   Serial.begin(9600);
+//   pinMode(outputPin, OUTPUT);
+//   digitalWrite(outputPin, LOW);
+//   pinMode(inputPin, INPUT);
+//   analogReadResolution(14);
+
+//   float raw1 = 16383 * (0.004 * resistor) / 5;
+//   float raw2 = 16383 * (0.02 * resistor) / 5;
+//   a = (temp_2 - temp_1) / (raw2 - raw1);
+//   b = temp_2 - a * raw2;
+// }
+
+// void loop() {
+//   // Serial.print("Pin 3: ");
+//   // Serial.print(digitalRead(outputPin));
+//   // Serial.print(" | Pin 4: ");
+//   // Serial.println(digitalRead(inputPin));
+
+//   if (digitalRead(inputPin) == HIGH) {
+//     int rawValue = analogRead(tempPin);
+//     float temperature = rawValue * a + b;
+
+//     tempWindow.push(temperature);
+//       //performs running average with n samples (max 20 samples)
+//       float sum = 0;
+//       for (int i = 0; i < tempWindow.size(); i++) {
+//         sum += tempWindow[i];
+//       }
+//       float average = sum / tempWindow.size();
+//       if (tempWindow.size() >= 20){
+//         Serial.print("log,");
+//         Serial.println(average);
+//         //flag to return control to Duet Board
+//         if (average <= max_temp && average >= min_temp) {
+//           Serial.println("done");
+//           // stop_tracking = true;
+//           tempWindow.clear();
+//           digitalWrite(outputPin, HIGH);
+//           delay(100);
+//           digitalWrite(outputPin, LOW);
+//           currentLayer += 1;
+//         } else if (average < min_temp) {
+//           Serial.println("abort");
+//           tempWindow.clear();
+//         }
+//         if (tempWindow.size() >= 20){
+//           tempWindow.shift();
+//         }
+//       }
+//       delay(200);
+//   }
+// }
+
+
 #include <Arduino.h>
 #include <CircularBuffer.h>
 
 CircularBuffer<float, 20> tempWindow;
 
 const int tempPin = A0;
-const float resistor = 263.6;
+// const float resistor = 263.6;
 float a;
 float b;
 
@@ -27,8 +104,8 @@ void setup() {
   pinMode(inputPin, INPUT);
   analogReadResolution(14);
 
-  float raw1 = 16383 * (0.004 * resistor) / 5;
-  float raw2 = 16383 * (0.02 * resistor) / 5;
+  float raw1 = 0;
+  float raw2 = 16383;
   a = (temp_2 - temp_1) / (raw2 - raw1);
   b = temp_2 - a * raw2;
 }
